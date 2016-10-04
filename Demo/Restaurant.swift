6d84f5b21dafac33d46121212cf26b1aeb017e66
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-04 14:52:41 +0200

    Added a Restaurant model class

---
+++ Demo/Restaurant.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant.swift
+//  Demo
+//
+//  Created by Ludovic Ollagnier on 04/10/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+import Foundation
+
+struct Restaurant {
+
+    let name: String
+    let address: String
+    var isFast: Bool
+    var grade: Float
+}
