09082c744dd7cc61b90fca97ee3f2dbdf32e75ca
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-04 14:13:51 +0200

    Basic outlets and actions creation

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var fastSwitch: UISwitch!
+    @IBOutlet weak var gradeSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func createRestaurant(_ sender: UIButton) {
+    }
 
 }
 
